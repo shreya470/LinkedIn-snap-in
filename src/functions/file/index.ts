f86baef3e3dd6bc4995bf4d65b1a94b36ea1e6f1
shreya470 <shreyan20@iitk.ac.in>
2023-07-19 21:43:01 +0530

    Added user data and user posts data to csv file

--- src/functions/file/index.ts
+++ src/functions/file/index.ts
@@ -1,54 +1,48 @@
 import fs from 'fs';
 import csv from 'csv-parser';
+import { type } from 'os';
+let base_url = "https://api.data365.co/v1.1/linkedin"
+let access_token="ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SnpkV0lpT2lKSmJtUnBZVzVKYm5OMGFYUjFkR1Z2WmxSbFkyaHViMnh2WjNsTFlXNXdkWElpTENKcFlYUWlPakUyT0RnMk16UXdOakF1TnpnME1UazJObjAuRWw1YllMTWtwbzdTRDNuVGRmSXlDZUtDMXhWSTgxbllLWm5QSE1Va19BYw=="
 
-async function readCSVFile(filePath: string): Promise<object[]> {
-  const results: object[] = [];
+function readCSVFile(filePath: string): string | null {
+  try {
+    const csvData = fs.readFileSync(filePath, 'utf8');
+    return csvData;
+  } catch (err) {
+    console.error('Error reading the file:', err);
+    return null;
+  }
+}
 
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(filePath)
-      .pipe(csv())
-      .on('data', (data) => results.push(data))
-      .on('end', () => resolve(results))
-      .on('error', (error) => reject(error));
+function syncWriteFile(filePath: string, data: any) {
+  fs.writeFileSync(filePath, data, {
+    flag: 'a+',
   });
 }
-export const run = async (events: any[]): Promise<Record<string, any>> => {
-  console.info(events);
 
-  const filePath = 'src\\functions\\file\\accountsProd.csv';
-
-  readCSVFile(filePath)
-  .then((data) => {
-    console.log(data); 
-  })
-  .catch((error) => {
-    console.error(error); 
-  });
-  
-  console.log(events)
-  readCSVFile(filePath)
-  .then((data) => {
-    console.log(data);
-    for (let item of data) {
-  
-      for (let item of data) {
-  const updateTask = 'https://api.data365.co/v1.1/linkedin/member/${event}/update?load_activities=true&max_activities=50&access_token=ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SnpkV0lpT2lKSmJtUnBZVzVKYm5OMGFYUjFkR1Z2WmxSbFkyaHViMnh2WjNsTFlXNXdkWElpTENKcFlYUWlPakUyT0RnMk16UXdOakF1TnpnME1UazJObjAuRWw1YllMTWtwbzdTRDNuVGRmSXlDZUtDMXhWSTgxbllLWm5QSE1Va19BYw==';
-  try {
-    const resp = await fetch(updateTask, {
+function updatetask(user: string){
+  const updateTask = base_url + "/member/" + user + "/update?load_activities=true&max_activities=50&access_token=" + access_token;
+    fetch(updateTask, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
         'accept': 'application/json, text/plain, */*',
       }  
-     
-    });
-  }
-  catch (error) {
-    console.log('Failed to post to timeline: ', error);
-  }
+      
+    })
+    .then(async data =>{
+      let datanew = await data.json();
+      console.log(datanew.data.task_id)
+    })
+    .catch(error => 
+      console.log('error', error)
+    );
+}
 
-  const getStatus ='https://api.data365.co/v1.1/linkedin/member/${event}/update?access_token=ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SnpkV0lpT2lKSmJtUnBZVzVKYm5OMGFYUjFkR1Z2WmxSbFkyaHViMnh2WjNsTFlXNXdkWElpTENKcFlYUWlPakUyT0RnMk16UXdOakF1TnpnME1UazJObjAuRWw1YllMTWtwbzdTRDNuVGRmSXlDZUtDMXhWSTgxbllLWm5QSE1Va19BYw==';
-  for (;;) {
+async function getstatus(user: string): Promise<string>{
+  const getStatus =base_url + "/member/" + user + "/update?access_token=" + access_token;
+  let max_tries = 200
+  for (let tries  = 0; tries < max_tries; tries++) {
     try {
       const resp = await fetch(getStatus, {
       method: 'GET',
@@ -59,20 +53,42 @@ export const run = async (events: any[]): Promise<Record<string, any>> => {
       });
       const response = await resp.json();
       const status = response.data.status;
-    
-      if (status === 'pending') { 
+      if (status === 'pending' || status === 'created') { 
       console.log('Status is pending. Calling API again...');
       continue;
-      } else if (status === 'finished' || status === 'created') {
-        break;
+      } else if (status === 'finished') {
+        console.log(status)
+        return status
       }
     }
     catch (error) {
       console.log('Failed to post to timeline: ', error);
+      return "Error occured!!"
     }
+    tries++;
   }
-  const getCachedData ='https://api.data365.co/v1.1/linkedin/member/${event}?access_token=ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SnpkV0lpT2lKSmJtUnBZVzVKYm5OMGFYUjFkR1Z2WmxSbFkyaHViMnh2WjNsTFlXNXdkWElpTENKcFlYUWlPakUyT0RnMk16UXdOakF1TnpnME1UazJObjAuRWw1YllMTWtwbzdTRDNuVGRmSXlDZUtDMXhWSTgxbllLWm5QSE1Va19BYw==';
-  try{
+  return "Max tries reached"
+}
+
+export const run = async (events: any[]): Promise<Record<string, any>> => {
+  console.info(events);
+  const filePath = 'src\\functions\\file\\accountsProd.csv';
+  const responsePath = 'src\\functions\\file\\response.csv';
+  let csvContent = readCSVFile(filePath);
+  // syncWriteFile(responsePath, "hello")
+  var users = [""]
+  if(csvContent != null){
+    users = csvContent.split(",\r\n")
+    console.log(users)
+  }
+  for (let i = 0; i < users.length; i++) {
+    updatetask(users[i])
+    let status = await getstatus(users[i])
+    console.log(status)
+
+    const getCachedData = base_url + "/member/" + users[i] + "?access_token=" + access_token;
+    console.log(getCachedData)
+    try{
       const resp = await fetch(getCachedData,{
         method: 'GET',
         headers: {
@@ -82,63 +98,48 @@ export const run = async (events: any[]): Promise<Record<string, any>> => {
       });
       if (resp.ok) {
         console.log("Successfully got data.");
-        let respJsonBody = await resp.json()
-  
-        //console.log('shreya',respJsonBody.data)
-        
-        //console.log('Positions:', respJsonBody.data.positions);
-        //console.log('Connection Count:', respJsonBody.data.connections_count);
-        //console.log('Followers Count:', respJsonBody.data.followers_count);
-       // console.log('Location:', respJsonBody.data.location_name);
-  
-        
-        
-
-  }
-  }
-  catch (error) {
-    console.log('Failed to get user data: ', error);
-  }
-  const getCachedPosts = 'https://api.data365.co/v1.1/linkedin/member/${event}/activity/created_post/posts?order_by=date_desc&access_token=ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SnpkV0lpT2lKSmJtUnBZVzVKYm5OMGFYUjFkR1Z2WmxSbFkyaHViMnh2WjNsTFlXNXdkWElpTENKcFlYUWlPakUyT0RnMk16UXdOakF1TnpnME1UazJObjAuRWw1YllMTWtwbzdTRDNuVGRmSXlDZUtDMXhWSTgxbllLWm5QSE1Va19BYw==';
-  let resp = undefined
-  try{
-    resp = await fetch(getCachedPosts,{
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json, text/plain, */*',
+        let response = await resp.json();
+        let temp = "Full name" + "," + "Connection Count" + "," + "Follower Count" + ","
+        for (let idx = 0; idx < response.data.positions.length; idx++){
+          temp = temp + "Company " + idx.toString() + "," + "Timeline" + idx.toString() + ","
+        }
+        syncWriteFile(responsePath, temp +"\n")
+        temp = ""
+        for (let idx = 0; idx < response.data.positions.length; idx++){
+          temp = temp+response.data.positions[idx].company.name+","+response.data.positions[idx].date_text+","
+        }
+        syncWriteFile(responsePath, response.data.full_name + "," + response.data.connections_count + "," + response.data.followers_count + "," + temp + "\n")
       }
-    });
-   // if (resp.ok) {
-      //console.log("Successfully got data.");
-      //let respJsonBody = await resp.json()
+    }
+    catch (error) {
+      console.log('Failed to get user data: ', error);
+    }
 
-     // console.log('shreya',respJsonBody.data.items)
-      //const items=respJsonBody.data.items
-      //const links =[]
-      //console.log(items[0])
-      //for(let item of items)
-      //{ 
-        //links.push({url:item.url})
-       // links.push({name : item.auther_username, url : item.url, likes : item.likes})
-
-      //}
-        //console.log(links)
-
-//}
+    const getCachedPosts = base_url + "/member/" + users[i] + "/activity/created_post/posts?order_by=date_desc&max_page_size=100&access_token=" + access_token;
+    try{
+      const resp = await fetch(getCachedPosts,{
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json, text/plain, */*',
+        }
+      });
+      if (resp.ok){
+        console.log("Successfully got data.");
+        let response = await resp.json();
+        syncWriteFile(responsePath, "Url,Comment Count,Created Time,Empathy Count,Entertainment Count,Interest Count,Like Count,Praise Count,Share Count,View Count,\n")
+        for (let idx = 0; idx < response.data.items.length; idx++){
+          let temp = ""
+          temp = temp + response.data.items[idx].url + "," + response.data.items[idx].comments_count + "," + response.data.items[idx].created_time + "," + response.data.items[idx].empathy_count + "," + response.data.items[idx].entertainment_count + "," + response.data.items[idx].interest_count + "," + response.data.items[idx].like_count + "," + response.data.items[idx].praise_count + "," + response.data.items[idx].shares_count + "," + response.data.items[idx].views_count + ","
+          syncWriteFile(responsePath, temp +"\n")
+        } 
+        
+      }
+    }
+    catch (error) {
+      console.log('Failed to get user data: ', error);
+    }
   }
-  catch (error) {
-    
-    console.error('API error:', error); 
-  }
-  //response = resp?.json
-  //for item in resp.data
-  
-    
-//} 
-    
-
-
   return { 'input_event': events[0] };
 }
 export default run;
